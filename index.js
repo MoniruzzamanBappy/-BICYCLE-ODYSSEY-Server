a9c55e8f143d7117bcfd1a126347b86852b146ad
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-05-22 15:16:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@bicycle-odyssey.snj10.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db("bicycle_odyssey").collection("parts");
+    app.get("/parts", async (req, res) => {
+      const result = await partsCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello bicycle odyssey!");
+});
+
+app.listen(port, () => {
+  console.log(`bicycle app listening on port ${port}`);
+});
