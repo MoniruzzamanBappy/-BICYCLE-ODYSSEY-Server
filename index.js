3b3a9587b09f187de1b68e79742fd0be76434661
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-05-22 23:12:48 +0600

    order added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,8 +19,22 @@ async function run() {
   try {
     await client.connect();
     const partsCollection = client.db("bicycle_odyssey").collection("parts");
+    const orderedCollection = client.db("bicycle_odyssey").collection("orderd");
     app.get("/parts", async (req, res) => {
       const result = await partsCollection.find().toArray();
+      res.send(result);
+    });
+    // get one tools
+    app.get("/parts/:_id", async (req, res) => {
+      const _id = req.params._id;
+      const query = {_id: ObjectId(_id)}
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+    // add order
+    app.post("/ordered", async (req, res) => {
+      const ordered = req.body;
+      const result = await orderedCollection.insertOne(ordered);
       res.send(result);
     });
   } finally {
