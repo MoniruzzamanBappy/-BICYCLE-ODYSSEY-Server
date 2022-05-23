e255837df788e5b4f0950ff34cd5420ee78f075f
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-05-23 15:12:06 +0600

    admin

--- index.js
+++ index.js
@@ -79,7 +79,6 @@ async function run() {
 
     app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
-      console.log(email)
       const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === "admin";
       res.send({ admin: isAdmin });
